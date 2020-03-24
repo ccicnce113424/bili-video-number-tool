4970d47ba29980aa0d3ee9c6175b5e514c76be0a
ccicnce113424 <30774232+ccicnce113424@users.noreply.github.com>
2020-03-24 11:42:36 +0800

    上传

---
+++ Example.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Example {
+    public static void main(String[] args) {
+        for(String arg : args) {
+            if (arg != null) {
+                if(!arg.isEmpty()) {
+                    System.out.println(trans(arg));
+                }
+            }
+        }
+        System.out.println("输入Q退出");
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            String in = scanner.nextLine();
+            if(in.equals("Q") || in.equals("q")) {
+                break;
+            }
+            System.out.println(trans(in));
+        }
+        scanner.close();
+    }
+
+    public static String trans(String number) {
+        if(number.startsWith("av")) {
+            return biliVideoNumberTool.a2b(number);
+        } else if (number.matches("[0-9]+")) {
+            return biliVideoNumberTool.a2b(("av" + number));
+        } else if(number.startsWith("BV")) {
+            return biliVideoNumberTool.b2a(number);
+        } else return "不是AV或BV号！";
+    }
+}
---
+++ biliVideoNumberTool.java
@@ -0,0 +1,26 @@
+public class biliVideoNumberTool {
+    public static final long xor = 177451812;
+    public static final long add = 8728348608L;
+    public static final String table = "fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF";
+    public static final int[] s = {11, 10, 3, 8, 4, 6};
+    
+    // av应为以av开头，后面跟纯十进制数字的字符串,如av810872
+    public static String a2b (String av) {
+        long x = Long.parseLong(av.substring(2));
+        x = (x ^ xor) + add;
+        char[] r = "BV1  4 1 7  ".toCharArray();
+        for(int i = 0; i < 6; i++) {
+            r[s[i]] = table.charAt((int) (x / (long) Math.pow(58, i) % 58));
+        }
+        return String.valueOf(r);
+    }
+
+    // bv应为BV开头,后跟十位包含大小写字母和数字的字符串,如BV17x411w7KC
+    public static String b2a (String bv) {
+        long r = 0;
+        for(int i = 0; i < 6; i++) {
+            r += ((long)table.indexOf(bv.charAt(s[i]))) * ((long)Math.pow(58, i));
+        }
+        return "av" + ((r - add) ^ xor);
+    }
+}
